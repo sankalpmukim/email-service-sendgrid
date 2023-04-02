c0043e1345eb533fbdb98188e085c1c6f566ab33
Sankalp Mukim <sankalpmukim@gmail.com>
2023-04-02 23:55:20 +0530

    project work

---
+++ main.js
@@ -0,0 +1,99 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const sgMail = require("@sendgrid/mail");
+const mongoose = require("mongoose");
+const app = express();
+
+// Connect to MongoDB database using Mongoose
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+const db = mongoose.connection;
+
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
+db.once("open", () => console.log("MongoDB connected successfully"));
+
+// Define schema for email logs
+const emailSchema = new mongoose.Schema({
+  to: String,
+  subject: String,
+  message: String,
+  recipients: [String],
+  type: String,
+  createdAt: { type: Date, default: Date.now },
+});
+
+const Email = mongoose.model("Email", emailSchema);
+
+// Set up SendGrid API Key and from email address from environment variables
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+const fromEmail = process.env.FROM_EMAIL;
+
+app.use(bodyParser.json());
+
+// Transactional email endpoint (one to one)
+app.post("/send-email", async (req, res) => {
+  try {
+    const { to, subject, message } = req.body;
+
+    const msg = {
+      to,
+      from: fromEmail,
+      subject,
+      text: message,
+    };
+
+    await sgMail.send(msg);
+
+    // Log email data to MongoDB database
+    const email = new Email({
+      to,
+      subject,
+      message,
+      type: "transactional",
+    });
+
+    await email.save();
+
+    res.status(200).json({ message: "Email sent successfully!" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Something went wrong" });
+  }
+});
+
+// Marketing email endpoint (one to many)
+app.post("/send-bulk-email", async (req, res) => {
+  try {
+    const { subject, message, recipients } = req.body;
+
+    const msg = {
+      to: recipients,
+      from: fromEmail,
+      subject,
+      html: message,
+    };
+
+    await sgMail.sendMultiple(msg);
+
+    // Log email data to MongoDB database
+    const email = new Email({
+      recipients,
+      subject,
+      message,
+      type: "marketing",
+    });
+
+    await email.save();
+
+    res.status(200).json({ message: "Bulk email sent successfully!" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Something went wrong" });
+  }
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`Server started on port ${process.env.PORT || 3000}`);
+});
